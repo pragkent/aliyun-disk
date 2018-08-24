c257316ad46947c6d71752d4cfcb15df48be75b9
Kent Wang <kent.wang@lixinchuxing.com>
2018-08-24 19:56:24 +0800

    Bump to 1.2.0

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package main
 
 const Name string = "aliyun-disk"
-const Version string = "1.1.0"
+const Version string = "1.2.0"
 
 // GitCommit describes latest commit hash.
 // This value is extracted by git command when building.
